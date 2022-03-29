84bff48a087bc8a862ccecd0c1e7fc0197f073d1
Vinicius Menezes <vini.ads13@outlook.com>
2022-03-29 14:24:29 -0300

    :pencil: add the forms for db can post the datas

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
@@ -11,8 +12,22 @@ interface newTransationModalProps{
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}:newTransationModalProps){
+  const [title,setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
+  function handleCreateNewTransaction(event:FormEvent) {
+    event.preventDefault();
+
+    const data = console.log({
+      title,
+      value,
+      category,
+      type});
+
+      api.post('/transactions', data)
+  }
 
   return (
     <Modal 
@@ -29,12 +44,21 @@ export function NewTransactionModal({isOpen, onRequestClose}:newTransationModalP
             <img src={closeImg} alt="fechar modal" />
           </button>
 
-          <Container>
+          <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar transação</h2>
 
-          <input placeholder="Título" />
+          <input 
+          placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          />
           
-          <input type="number" placeholder="Valor" />
+          <input 
+          type="number" 
+          placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))} 
+          />
 
           <TransactionTypeContainer>
             <RadioBox 
@@ -64,7 +88,10 @@ export function NewTransactionModal({isOpen, onRequestClose}:newTransationModalP
 
           </TransactionTypeContainer>
 
-          <input placeholder="Categoria" />
+          <input 
+          placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}/>
 
           <button type="submit">
             Cadastrar
